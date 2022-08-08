de63d4bee5aabcb560bdb6e47fbffde4a3df952f
BomsTechnology <marcsigha@gmail.com>
2022-08-08 12:07:48 +0100

    first commit

---
+++ src/router/index.ts
@@ -0,0 +1,65 @@
+import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
+import "vue-router";
+
+declare module "vue-router" {
+  interface RouteMeta {
+    title: string;
+    isAdmin?: boolean;
+    requiresAuth?: boolean;
+  }
+}
+const websiteName: String = "Dr. SIGHA Odette";
+const routes: Array<RouteRecordRaw> = [
+  {
+    path: "/",
+    name: "home",
+    component: () => import("@/views/front/Home.vue"),
+    meta: {
+      title: "Médécine Esthétique au Cameroun | " + websiteName,
+    },
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+  scrollBehavior(to, from, savedPosition) {
+    if (to.hash) {
+      return {
+        el: to.hash,
+        behavior: "smooth",
+      };
+    } else if (savedPosition) {
+      return savedPosition;
+    } else {
+      return { top: 0 };
+    }
+  },
+});
+
+// router.beforeEach(async (to, from, next) => {
+//   const userStore = useUserStore();
+//   if (to.meta.requiresAuth) {
+//     if (await userStore.getCurrentUser()) {
+//       if (to.meta.isAdmin) {
+//         if (userStore.currentUser.type == "admin") {
+//           next();
+//         } else {
+//           next({ name: "home" });
+//         }
+//       } else {
+//         next();
+//       }
+//     } else {
+//       next({ name: "login" });
+//     }
+//   } else {
+//     next();
+//   }
+// });
+
+router.afterEach((to) => {
+  document.title = to.meta.title;
+});
+
+export default router;
